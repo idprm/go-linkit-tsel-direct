e917ba028ea0fdc5ba6d0cb3f919a139a1f6a9e4
Indra Pramana <idprama@gmail.com>
2024-04-30 06:56:55 +0700

    add new tz

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/idprm/go-linkit-tsel/cmd"
+import (
+	"time"
+
+	"github.com/idprm/go-linkit-tsel/cmd"
+	"github.com/idprm/go-linkit-tsel/internal/utils"
+)
+
+var (
+	APP_TZ string = utils.GetEnv("APP_TZ")
+)
 
 func main() {
+
+	loc, _ := time.LoadLocation(APP_TZ)
+	time.Local = loc
+
 	cmd.Execute()
 }
